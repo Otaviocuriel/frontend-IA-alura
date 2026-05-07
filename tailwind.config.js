d027713c68c25c94393daf7072c0e3fabd2dcb95
Otaviocuriel <otavio.curiel345@gmail.com>
2026-05-07 10:32:09 -0300

    Add Tailwind fade animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-up": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-up": "fade-up 620ms ease-out",
+      },
+    },
   },
   plugins: [],
-}
+};
